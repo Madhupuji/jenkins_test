a4de823990ac2d1264db1f0b8e36b306687e2e35
Madhupuji <109216779+Madhupuji@users.noreply.github.com>
2024-11-26 12:33:54 +0530

    Update AppTest.java

--- api-gateway/src/test/java/com/nexca/apigateway/AppTest.java
+++ api-gateway/src/test/java/com/nexca/apigateway/AppTest.java
@@ -8,7 +8,7 @@ import junit.framework.TestSuite;
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+    extends TestCase  //--- modify
 {
     /**
      * Create the test case
